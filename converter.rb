b5b2598e7c8eba13e498566da772a1e4dae8b446
Katherine Lynch <katherly@upenn.edu>
2017-05-12 14:13:52 -0400

    Modified to take in yml arguments, added yml example file

--- converter.rb
+++ converter.rb
@@ -1,27 +1,43 @@
 #!/bin/ruby
 
 require 'mini_magick'
+require 'yaml'
 require 'pry'
 
 
-def mogrify_actions(options = {})
+def mogrify_actions(options = {}, image)
   mogrify = MiniMagick::Tool::Mogrify.new
-  mogrify.resample(options[:ppi]) if options[:ppi].present?
-  mogrify << converted_image
+  mogrify.resample(options['ppi']) unless options['ppi'].nil?
+  mogrify << image
 end
 
-original_location = '/Users/kate/Documents/Projects/tifs'
-coverted_location = '/Users/kate/Documents/Projects/jp2s'
+def populate_mogrify_options(config)
+  options = {}
+  mogrify_keys = ['ppi']
+  mogrify_keys.each do |key|
+    options[key] = config[key]
+  end
+  return options
+end
 
-original_format = 'tif'
-converted_format = 'jp2'
+relative_config_path = 'config.yml'
 
+abort('Configuration file not present.  See README for instructions.') unless File.exist?(relative_config_path)
 
+config = YAML.load_file(relative_config_path)
+
+original_location = config['original_location']
+converted_location = config['converted_location']
+
+original_format = config['original_format']
+converted_format = config['converted_format']
+
+mogrify_options = populate_mogrify_options(config)
 
 Dir.glob("#{original_location}/*.#{original_format}").each do |file|
   image = MiniMagick::Image.open(file)
-  converted_image = "#{coverted_location}/#{File.basename(file, '.*')}.#{converted_format}"
+  converted_image = "#{converted_location}/#{File.basename(file, '.*')}.#{converted_format}"
   image.format "#{converted_format}"
   image.write "#{converted_image}"
-  mogrify_actions(mogrify_options) if mogrify_options.present?
+  mogrify_actions(mogrify_options, converted_image) unless mogrify_options.empty?
 end
